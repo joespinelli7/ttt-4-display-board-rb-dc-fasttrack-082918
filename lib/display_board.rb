803563718810a7ba6a8f61a4fdbd737641e4d027
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-09-29 19:05:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   separator = "|"
   dash = "-----------"
   puts "#{board[0]}#{separator}#{board[1]}#{separator}#{board[2]}"
