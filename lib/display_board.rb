30d95d542185ac0f9d0d55e45e221dd2ee5f5ee9
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-09-29 19:09:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,11 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   separator = "|"
   dash = "-----------"
-  puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]}"
-  puts " #{dash}"
-  puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]}"
-  puts " #{dash}"
-  puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
+  puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+  puts "#{dash}"
+  puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+  puts "#{dash}"
+  puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
 
 display_board(board)
